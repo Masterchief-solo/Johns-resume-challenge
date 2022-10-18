6e74090d4dd7b2bb12b39290128c66de9af0bc13
John Cook <90234449+Masterchief-solo@users.noreply.github.com>
2022-10-18 11:22:14 -0500

    Update display-count.js

--- display-count.js
+++ display-count.js
@@ -1,6 +1,6 @@
 <html>
 <script> 
-	fetch('https://h2j0r15521.execute-api.us-east-2.amazonaws.com/DEV')
+	fetch('https://h1h2h31100000.execute-api.us-east-1.amazonaws.com/BETA')
     	.then(response => response.json())
         .then((data) => {document.getElementById('visits').innerText = data.body})
 
